e75a1438077468d52a722a7bfc050e4a8113a63a
albapc <alba.perez.ce@gmail.com>
2019-04-03 19:44:15 +0200

    commit final

---
+++ src/boletin27/Boletin27.java
@@ -0,0 +1,11 @@
+
+package boletin27;
+
+public class Boletin27 {
+
+    public static void main(String[] args) {
+        Calculadora calc = new Calculadora();
+        calc.setVisible(true);
+    }
+    
+}
